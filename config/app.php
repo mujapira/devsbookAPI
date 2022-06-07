2d7315fb80ae9c9e6b598918ab0cb87b5e3d9086
Mujapira <malmasmello2@gmail.com>
2022-06-07 19:04:16 -0300

    Routes

--- config/app.php
+++ config/app.php
@@ -183,8 +183,8 @@ return [
         Illuminate\View\ViewServiceProvider::class,
 
         /*
-         * Package Service Providers...
-         */
+        * Package Service Providers...
+        */
 
         /*
          * Application Service Providers...
@@ -194,6 +194,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        // intervention
+        Intervention\Image\ImageServiceProvider::class,
 
     ],
 
@@ -209,7 +211,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
-        // 'ExampleClass' => App\Example\ExampleClass::class,
+        'Image' => Intervention\Image\Facades\Image::class
     ])->toArray(),
 
 ];
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,27 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/logout', [AuthController::class, 'logout']);
+Route::post('/auth/refresh', [AuthController::class, 'refresh']);
+Route::post('/user', [AuthController::class, 'create']);
+
+Route::put('/user', [UserController::class, 'update']);
+Route::post('/user/avatar', [AuthController::class, 'updateAvatar']);
+Route::post('/user/cover', [AuthController::class, 'updateCover']);
+
+Route::get('/feed', [FeedController::class, 'read']);
+Route::get('/user/feed', [FeedController::class, 'userFeed']);
+Route::get('/user/{id}/feed', [FeedController::class, 'userFeed']);
+
+Route::get('/user', [UserController::class, 'read']);
+Route::get('/user/{id}', [UserController::class, 'read']);
+
+Route::post('/feed', [FeedController::class, 'create']);
+
+Route::post('/post/{id}/like', [PostController::class, 'like']);
+Route::post('/post/{id}/comment', [PostController::class, 'comment']);
+
+Route::get('/search', [SearchController::class, 'search']);
